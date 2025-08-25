c491947d3c2a3d59f5c2128b39ad9264e04c5886
Tijani Basit <tijanibwebdev@gmail.com>
2025-08-25 14:29:54 -0700

    Fix sidear routing

--- src/components/ResponsiveLayout.jsx
+++ src/components/ResponsiveLayout.jsx
@@ -66,31 +66,31 @@ const ResponsiveLayout = () => {
                   <RiAlipayLine className='ic'/>
                   <span>Health & Beauty</span>
                 </NavLink>
-                <NavLink to={`/home-office/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
+                <NavLink to={`/phones-tablets/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
                   <RiHome2Line className='ic'/>
                   <span>Home & Office</span>
                 </NavLink>
-                <NavLink to={`/electronics/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
+                <NavLink to={`/appliances/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
                   <RiRadio2Line className='ic'/>
                   <span>Electronics</span>
                 </NavLink>
-                <NavLink to={`/fashion/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
+                <NavLink to={`/health-beauty/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
                   <RiShirtLine className='ic'/>
                   <span>Fashion</span>
                 </NavLink>
-                <NavLink to={`/supermarket/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
+                <NavLink to={`/health-beauty/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
                   <RiAppleLine className='ic'/>
                   <span>Supermarket</span>
                 </NavLink>
-                <NavLink to={`/computing/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
+                <NavLink to={`/appliances/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
                   <RiTv2Line className='ic'/>
                   <span>Computing</span>
                 </NavLink>
-                <NavLink to={`/gaming/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
+                <NavLink to={`/phones-tablets/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
                   <RiGamepadLine className='ic'/>
                   <span>Gaming</span>
                 </NavLink>
-                <NavLink to={`/musical-instruments/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
+                <NavLink to={`/appliances/`} className="cat" onClick={() => setMobileSidebarOpen(false)}>
                   <RiMusicAiLine className='ic'/>
                   <span>Musical Instruments</span>
                 </NavLink>
